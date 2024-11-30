93447a422d59f3b9adcc3820ae126d96be403ecc
sathesh84 <sathesh84@gmail.com>
2024-11-30 22:32:11 +0530

    add security to addActivity

--- addActivity.php
+++ addActivity.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    // Redirect to login page
+    header("Location: login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
